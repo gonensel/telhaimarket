911b6571df8eba649b9b528e53b13d5ad19a5a53
gonensel <gonenselner@gmail.com>
2022-03-10 22:21:51 +0200

    register almost done add update user in db

--- app/src/main/java/com/example/telhaimarket/MainActivity.java
+++ app/src/main/java/com/example/telhaimarket/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.telhaimarket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        register=findViewById(R.id.register_button);
+        register.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this,Register.class));
+                finish();
+            }
+        });
     }
+
 }
