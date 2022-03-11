08581f2adec0e5c41e4a332ce4f96e5fc7e4ca0a
RazRabino <100515676+RazRabino@users.noreply.github.com>
2022-03-11 16:51:18 +0200

    edit profile page activity -- (Raz)

--- app/src/main/java/com/example/telhaimarket/ProfileEditPage.java
+++ app/src/main/java/com/example/telhaimarket/ProfileEditPage.java
@@ -10,5 +10,9 @@ public class ProfileEditPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_edit_page);
+        getSupportActionBar().setTitle("Edit Profile");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
     }
 }
