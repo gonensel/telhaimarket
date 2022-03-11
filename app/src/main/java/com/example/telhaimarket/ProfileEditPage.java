2c28360c94a2b864e2c69e62b175ad812b545a23
HadasahS <62838748+HadasahS@users.noreply.github.com>
2022-03-11 16:02:39 +0200

    Add Edit Page

---
+++ app/src/main/java/com/example/telhaimarket/ProfileEditPage.java
@@ -0,0 +1,14 @@
+package com.example.telhaimarket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileEditPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_edit_page);
+    }
+}
