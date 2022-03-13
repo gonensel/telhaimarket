2b6e0406ba313bb6d664dd2bcde023ec1c8e1683
gonensel <gonenselner@gmail.com>
2022-03-13 08:56:40 +0200

    a little fix in register Tost

--- app/src/main/java/com/example/telhaimarket/MainActivity.java
+++ app/src/main/java/com/example/telhaimarket/MainActivity.java
@@ -106,7 +106,6 @@ public class MainActivity extends AppCompatActivity {
                                     }
                                 }
                             }
-
                             @Override
                             public void onCancelled(@NonNull DatabaseError error) { }
                         });
